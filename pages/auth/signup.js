dd8e5e177c6548999d299c4380e9e660b9ec28a3
Yordi Ogando <yordiogando@gmail.com>
2021-04-28 21:47:34 -0400

    Initial signup implementation

---
+++ pages/auth/signup.js
@@ -0,0 +1,42 @@
+import axios from 'axios';
+import { useState } from 'react';
+
+export default () => {
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+
+	const onSubmit = async (event) => {
+		event.preventDefault();
+
+		const res = await axios.post('/api/users/signup', {
+			email,
+			password,
+		});
+
+		console.log(res.data);
+	};
+
+	return (
+		<form onSubmit={onSubmit}>
+			<h1>Sign up</h1>
+			<div className="form-group">
+				<label>Email Address</label>
+				<input
+					value={email}
+					onChange={(e) => setEmail(e.target.value)}
+					className="form-control"
+				/>
+			</div>
+			<div className="form-group">
+				<label>Password</label>
+				<input
+					value={password}
+					onChange={(e) => setPassword(e.target.value)}
+					type="password"
+					className="form-control"
+				/>
+			</div>
+			<button className="btn btn-primary">Sign up</button>
+		</form>
+	);
+};
