856e0d5e4b259ef1e162715d08c4409a07537155
Yordi Ogando <yordiogando@gmail.com>
2021-05-01 22:31:05 -0400

    Verify is user is logged in on SSR

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,29 @@
-export default () => {
+import axios from 'axios';
+
+const LandingPage = ({ currentUser }) => {
+	console.log(currentUser);
+
 	return <h1>Landing</h1>;
 };
+
+//Req is the request object, just like in express
+LandingPage.getInitialProps = async ({ req }) => {
+	let res;
+
+	if (typeof window === 'undefined') {
+		//Server
+		res = await axios.get(
+			'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+			{
+				headers: req.headers,
+			}
+		);
+	} else {
+		//Browser
+		res = await axios.get('/api/users/currentuser');
+	}
+
+	return res.data;
+};
+
+export default LandingPage;
