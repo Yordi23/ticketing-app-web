50d0072adbe3be4096b23ddf58d41ff34d789c8c
Yordi Ogando <yordiogando@gmail.com>
2021-05-01 23:35:54 -0400

    Moved verification of user signed in to _app and created header

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,31 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import buildClient from '../api/build-client';
+import Header from '../components/header';
 
-export default ({ Component, pageProps }) => {
-	return <Component {...pageProps} />;
+const AppComponent = ({ Component, pageProps, currentUser }) => {
+	return (
+		<div>
+			<Header currentUser={currentUser} />
+			<Component {...pageProps} />
+		</div>
+	);
 };
+
+AppComponent.getInitialProps = async ({ Component, ctx }) => {
+	const client = buildClient(ctx);
+	const { data } = await client.get('/api/users/currentuser');
+
+	let pageProps = {};
+	if (Component.getInitialProps) {
+		//Manually executes getInitialProps function from the page being rendered, this is because it does not
+		//executes automatically after setting a getInitialProps function inside of a custom AppComponent like this
+		pageProps = await Component.getInitialProps(ctx);
+	}
+
+	return {
+		pageProps,
+		currentUser: data.currentUser,
+	};
+};
+
+export default AppComponent;
